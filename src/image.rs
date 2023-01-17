81dcad0bfdec179f602243bcd3c72b1bba94b60c
Martin Kysel <code@martinkysel.com>
2023-01-17 12:10:05 -0300

    Add result types and tests of basic funcionality (#2)
    * add tests
    
    * bump toolchain
    
    * clippism

--- src/image.rs
+++ src/image.rs
@@ -1,27 +1,48 @@
 use crate::command::docker_exec;
-use serde::Deserialize;
+use crate::error;
+use crate::models::ImageSummary;
 
-#[derive(Deserialize, Debug)]
-struct ImageMetadata {
-    name: String,
-    tag: String,
+pub fn list_images() -> error::Result<Vec<ImageSummary>> {
+    let output = docker_exec(vec!["images", "--format", "{{json .}}"]).unwrap();
+    let images: Vec<ImageSummary> = output
+        .split('\n')
+        .filter(|part| !part.is_empty())
+        .map(serde_json::from_str)
+        .collect::<Result<Vec<ImageSummary>, serde_json::Error>>()?;
+
+    Ok(images)
 }
 
-pub fn list_images() -> String {
-    docker_exec(vec!["images"])
-}
-
-pub fn find_by_tag(component: &str, tag: &str) -> String {
-    let output = docker_exec(vec!["images", "--format", "'{{json .}}'"]);
-    let images: Vec<ImageMetadata> = serde_json::from_str(&output).expect("Failed to parse JSON");
+pub fn find_by_tag(component: &str, tag: &str) -> error::Result<Option<ImageSummary>> {
+    let images = list_images()?;
 
     for image in images {
-        if image.tag == tag && image.name.contains(component) {
-            return image.name;
+        if image.tag == tag && image.repository.contains(component) {
+            return Ok(Some(image));
         }
     }
-    format!(
-        "Tag {} not found for component {}, please build manually and try again",
-        tag, component
-    )
+
+    Ok(None)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::image::{find_by_tag, list_images};
+
+    #[test]
+    fn test_list_images() {
+        list_images().unwrap();
+    }
+
+    #[test]
+    fn test_find_alpine() {
+        let r = find_by_tag("alpine", "latest").unwrap();
+        assert!(r.is_some());
+    }
+
+    #[test]
+    fn test_find_nonexistant() {
+        let r = find_by_tag("non-existant", "local").unwrap();
+        assert!(r.is_none());
+    }
 }
