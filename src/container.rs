1d0f1f10e111a274d58206e3dcb24ec98f7e2ba9
Isaac Hartford <pianista1337@gmail.com>
2023-01-19 13:35:34 -0500

    updates

--- src/container.rs
+++ src/container.rs
@@ -25,14 +25,15 @@ pub struct Container {
     pub volumes: HashMap<String, String>,
     pub env: HashMap<String, String>,
     pub cmd: String,
+    pub port: usize,
+    pub blocking: bool
 }
 
 pub trait ContainerImpl {
     fn start(&self) -> String;
-    fn start_blocking(&self) -> String;
-    fn list_running(&self) -> String;
     fn get_image(&self) -> String;
     fn get_env(&self) -> Vec<String>;
+    fn get_port(&self) -> usize;
     fn get_volumes(&self) -> String;
     fn get_cmd(&self) -> String;
 }
@@ -46,25 +47,21 @@ pub fn list_running() -> String {
 }
 
 impl ContainerImpl for Container {
+
     fn start(&self) -> String {
         let mut cmd = vec!["run"];
         let img = self.get_image();
-        let ops = vec!["-d", img.as_str()];
         let e = self.get_env();
         let env: Vec<&str> = e.iter().map(|s| s.as_str()).collect();
-
+        if self.blocking {
+            cmd.extend(vec!["-a", "-rm"]);
+        } else {
+            cmd.extend(vec!["-d"]);
+        }
         cmd.extend(env);
-        cmd.extend(ops);
+        cmd.extend(vec![img.as_str()]);
 
         command::docker_exec(cmd).unwrap()
-    }
-
-    fn start_blocking(&self) -> String {
-        command::docker_exec(vec!["run", "-a", self.get_image().as_str()]).unwrap()
-    }
-
-    fn list_running(&self) -> String {
-        command::docker_exec(vec!["ps", "-a", "-f", "status=running"]).unwrap()
     }
 
     fn get_image(&self) -> String {
@@ -82,6 +79,10 @@ impl ContainerImpl for Container {
             env.push(format!("{}={}", key, value));
         }
         env
+    }
+
+    fn get_port(&self) -> usize {
+        self.port
     }
 
     fn get_volumes(&self) -> String {
